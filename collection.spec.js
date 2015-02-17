ce1eedf3285976f9c59c326508502b7ebb2eed7e
Dakota McCoy <dakotaraymccoy@gmail.com>
2015-02-17 17:11:56 -0500

    issue #36 throwing an assertion error. Checking for length of a task collection is returning the function written for length, not the length itself. Previously this error was caused by not having parentheses after calling the method, but we have them this time. The issue is on line 37. @skiadas

--- collection.spec.js
+++ collection.spec.js
@@ -11,3 +11,33 @@ Task = require('./task.js');
 TaskCollection = require('./collection.js');
 
 // ADD YOUR TESTS HERE
+
+describe("makeNewCollection constructor", function(){
+	beforeEach(function(){
+		c = TaskCollection.new();
+	});
+
+	it("is a function", function(){
+		expect(TaskCollection.new).to.not.throw(Error);
+		expect(TaskCollection.new).to.be.a("function");
+	});
+
+	it("values is an empty array", function(){
+
+	});
+});
+
+describe("proto methods", function(){
+	beforeEach(function(){
+		c = TaskCollection.new();
+		o = Task.new();
+	});
+
+	it("length, addOneTask, removeOneTask", function(){
+		expect(c.length()).to.equal(0);
+		c.addOneTask(o);
+		expect(c.length()).to.equal(1);
+		c.removeOneTask(o.id);
+		expect(c.length()).to.equal(0);
+	});
+});
