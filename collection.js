e53e416b1d0df59c52feab2f561eea17f7b3bb0f
Edwin Nartey <narteye15@hanover.edu>
2015-02-17 20:29:51 -0500

    fixed more eslint errors. Issue #35

--- collection.js
+++ collection.js
@@ -201,7 +201,7 @@ proto = {
 	},
 
 	concat: function(){
-		"use strict"
+		"use strict";
 		var index;
 		for (index = 0; index < arguments.length; index += 1){
 			this.addOneTask(arguments[ index ]);
