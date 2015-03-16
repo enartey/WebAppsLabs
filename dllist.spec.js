f38c9968617dcfe0d72e3ba5a723d83047e84c2d
Dakota McCoy <dakotaraymccoy@gmail.com>
2015-03-16 11:29:10 -0400

    push not working properly, see line 19 of test code for first error.

--- dllist.spec.js
+++ dllist.spec.js
@@ -3,7 +3,7 @@
  *
  * Test file for your history class
  */
-var expect, DLList, CmdHistory;
+var expect, DLList, CmdHistory, DL1, DL2, arg1 = 1, arg2 = 2;
 
 expect = require('./chai.js').expect;
 
@@ -11,3 +11,17 @@ DLList = require('./dllist.js');
 CmdHistory = require('./history.js');
 
 // ADD YOUR TESTS HERE
+
+describe("proto methods", function(){
+	beforeEach(function(){
+		DL1 = DLList.new();
+		DL2 = DLList.new();
+		DL2.push(arg1);
+		DL2.push(arg2);
+		DL2.push(3);
+	});
+
+	it("isEmpty", function(){
+		expect(DL1.isEmpty()).to.equal(true);
+	})
+});
