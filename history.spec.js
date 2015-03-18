5ccf3f5d6c7708a88e0485523b38ec4f557c2cf6
Dakota McCoy <dakotaraymccoy@gmail.com>
2015-03-18 11:47:49 -0400

    issue #91, but the before each hook is failing

--- history.spec.js
+++ history.spec.js
@@ -3,7 +3,7 @@
  *
  * Test file for your history class
  */
-var expect, DLList, CmdHistory;
+var expect, DLList, CmdHistory, H1, cmd;
 
 expect = require('./chai.js').expect;
 
@@ -29,3 +29,23 @@ function mockCommand() {
 }
 
 // ADD YOUR TESTS HERE
+
+describe("proto methods", function(){
+	/*Adds a new command to the history
+	to immediately follow "current".All
+	the commands that were following 
+	"current" must be removed, as they
+	no longer are redoable. Also, the
+	new command needs to be executed
+	(by calling its execute method).*/
+	beforeEach(function(){
+		cmd = mockCommand();
+		H1 = CmdHistory.new();
+		H1.add(cmd);
+	});
+
+	it("add() issue #91", function(){
+		expect(H1.current).to.equal(cmd);
+	});
+
+});
